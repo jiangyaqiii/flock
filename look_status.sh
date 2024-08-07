9183a05528b067093979b6546a12194c6d6e297d
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-08-07 20:52:37 +0800

    Update look_status.sh

--- look_status.sh
+++ look_status.sh
@@ -3,7 +3,7 @@
 PROCESS_NAME="llm-loss-validator"
 
 # 获取进程状态
-status=$(pm2 list | grep "$PROCESS_NAME" | awk '{print $5}')
+status=$(pm2 list | grep "$PROCESS_NAME" | awk '{print $18}')
 
 if [ "$status" == "online" ]; then
     echo "运行中"
